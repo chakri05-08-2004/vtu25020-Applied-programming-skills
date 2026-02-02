52bf85574c060ce15318a71405f5ef313d257d30
chakri05-08-2004 <vtu25020@veltech.edu.in>
2026-02-02 22:00:03 +0530

    Time: 20 ms (95.27%), Space: 80.8 MB (59.08%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,20 +1,25 @@
-import java.util.*;
-
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                // check distance between duplicate indices
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                }
+// Time Complexity : O(n)
+        // Base case...
+        if(nums == null || nums.length < 2 || k == 0)
+            return false;
+        int i = 0;
+        // Create a Hash Set for storing previous of k elements...
+        HashSet<Integer> hset = new HashSet<Integer>();
+        // Traverse for all elements of the given array in a for loop...
+        for(int j = 0; j < nums.length; j++) {
+            // If duplicate element is present at distance less than equal to k, return true...
+            if(!hset.add(nums[j])){
+                return true;
             }
-            // update last seen index
-            map.put(nums[i], i);
+            // If size of the Hash Set becomes greater than k...
+            if(hset.size() >= k+1){
+                // Remove the last visited element from the set...
+                hset.remove(nums[i++]);
+            }
         }
-
+        // If no duplicate element is found then return false...
         return false;
     }
 }
